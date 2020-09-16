f74863db09475c3eff409d9c37ca3e02d192e8b2
tjfruddnjs1 <tjfruddnjs12@naver.com>
2020-09-16 21:05:55 +0900

    Initial Commit (SeolKyungWon)

--- src/kr/ac/jbnu/se/awp/gitplay2/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay2/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("I'm Lim Jong Muk");
+		System.out.println("I'm MunHyunHo!!");
 	}
 }

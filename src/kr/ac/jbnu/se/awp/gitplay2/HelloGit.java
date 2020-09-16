ea2b45ad3baab787f0455f96cd45bab204b5f537
IOUIOU50 <57579709+IOUIOU50@users.noreply.github.com>
2020-09-16 20:56:17 +0900

    Test Lim Jong Muk

--- src/kr/ac/jbnu/se/awp/gitplay2/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay2/HelloGit.java
@@ -3,5 +3,6 @@ package kr.ac.jbnu.se.awp.gitplay2;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
+		System.out.println("I'm Lim Jong Muk");
 	}
 }

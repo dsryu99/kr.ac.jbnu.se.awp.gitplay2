86588834c0662a0f4209e8f85c0b32a6f3068460
unn04012 <unn04012@naver.com>
2020-09-17 12:08:54 +0900

    add comment

--- src/kr/ac/jbnu/se/awp/gitplay2/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay2/HelloGit.java
@@ -3,9 +3,9 @@ package kr.ac.jbnu.se.awp.gitplay2;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
-		System.out.println("I'm Lim Jong Muk");
-		System.out.println("I'm MunHyunHo!!");
+		System.out.println("I'm Lim Jong Muk");		
 		System.out.println("I'm Choi Jong Wan!");
 		System.out.println("I'm Choi sung woo!");
+		System.out.println("I'm MunHyunHo!!");
 	}
 }
